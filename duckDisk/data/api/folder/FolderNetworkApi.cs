0295a2227edbb040840d6c0cada5e4a0b55bb114
ISPr24-39_BeluakovDS <dan.bel.89@bk.ru>
2023-06-28 09:14:34 +0400

    add folder request api

--- duckDisk/data/api/folder/FolderNetworkApi.cs
+++ duckDisk/data/api/folder/FolderNetworkApi.cs
@@ -7,6 +7,8 @@ using System.Net.Http;
 using System.Web;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Windows.Controls;
+using duckDisk.data.api.user.dto;
 
 namespace duckDisk.data.api.folder
 {
@@ -34,6 +36,30 @@ namespace duckDisk.data.api.folder
             var json = response.Content.ReadAsStringAsync().Result;
 
             return JsonConvert.DeserializeObject<Paging<Folder>>(json);
+        }
+
+        public Folder Add(string name, int? folderId = null)
+        {
+            var token = localStorage.Get<JwtResponseDto>("jwt_response").AccessToken;
+
+            var builder = new UriBuilder($"{NetwokConstants.BASE_URL}/folders");
+
+            var query = HttpUtility.ParseQueryString(builder.Query);
+            query["folder_id"] = folderId.ToString();
+            query["name"] = name.ToString();
+            builder.Query = query.ToString();
+
+            var url = builder.ToString();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+
+            request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {token}");
+
+            var response = httpClient.SendAsync(request).Result;
+
+            var json = response.Content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<Folder>(json);
         }
     }
 }
